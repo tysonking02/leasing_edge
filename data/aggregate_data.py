9b1d2675a6a73899b652ac35a8204ff5bbab481f
tysonking02 <tyson.king@cortland.com>
2025-07-30 11:41:10 -0400

    updated data for 7/29

--- data/aggregate_data.py
+++ data/aggregate_data.py
@@ -17,17 +17,15 @@ for layout, col_name in layout_types.items():
 
 export_clients = export_clients[export_clients['client_status'].isin(['Prospect', 'Toured'])]
 
-# filter to last year of prospects
-export_clients = export_clients[pd.to_datetime(export_clients['created_at']) >= pd.Timestamp.now(tz="UTC") - pd.Timedelta(days=365)]
+# filter to last 6 months of prospects
+export_clients = export_clients[pd.to_datetime(export_clients['created_at']) >= pd.Timestamp.now(tz="UTC") - pd.Timedelta(days=180)]
 
 export_clients.to_csv('data/processed/export_clients.csv')
 export_clients.head(10).to_csv('export_clients_head.csv')
 
 export_group_assignment = pd.read_parquet('data/raw/export_group_assignment')
 
-print(export_group_assignment['client_status'].unique())
-
-export_group_assignment = export_group_assignment[export_group_assignment['client_status'].isin(['Prospect', 'Toured'])]
+export_group_assignment = export_group_assignment[export_group_assignment['clientid'].isin(export_clients['client_id'])]
 
 export_group_assignment.to_csv('data/processed/export_group_assignment.csv')
 export_group_assignment.head(10).to_csv('export_group_assignment_head.csv')
